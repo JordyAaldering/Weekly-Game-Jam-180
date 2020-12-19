384752ce3db10c2699216448556028e444d4000c
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-19 15:54:44 +0100

    Enemy Movement

---
+++ Assets/MoveTowardsPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MoveTowardsPlayer : StateMachineBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private Transform player;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Transform transform = animator.transform;
+        transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
---
+++ Assets/RotateAroundPlayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class RotateAroundPlayer : StateMachineBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float orbitDistance;
+    [SerializeField] private float orbitRotateSpeed;
+
+    private Transform player;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        Transform transform = animator.transform;
+        Vector3 anglePoint = LerpByDistance(player.position, transform.position, orbitDistance);
+        Vector3 targetPos = RotatePointAroundPivot(anglePoint, player.position);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+
+    private Vector3 LerpByDistance(Vector3 from, Vector3 to, float dist)
+    {
+        return dist * (to - from).normalized + from;
+    }
+
+    private Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot)
+    {
+        return Quaternion.Euler(0f, 0f, orbitRotateSpeed) * (point - pivot) + pivot;
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        
+    }
+}
