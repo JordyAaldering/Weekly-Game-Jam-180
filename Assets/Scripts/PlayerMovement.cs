b2ca3f272a04b2a748be8561d11c688d1ec9ed89
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-19 14:46:08 +0100

    Basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+	private Vector2 moveDir;
+    private Rigidbody2D rb;
+
+	private void Awake()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		moveDir = new Vector2(
+			Input.GetAxis("Horizontal"),
+			Input.GetAxis("Vertical")
+		);
+	}
+
+	private void FixedUpdate()
+	{
+		moveDir = Vector2.ClampMagnitude(moveDir, 1f);
+		rb.velocity = moveSpeed * moveDir;
+	}
+}
