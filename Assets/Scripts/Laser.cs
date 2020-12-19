adf8a555e27d65fa9714045b1bf24320d232748e
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-19 17:24:59 +0100

    Laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float lifeTime;
+
+	private void Update()
+	{
+		transform.position += moveSpeed * Time.deltaTime * transform.right;
+
+		lifeTime -= Time.deltaTime;
+		if (lifeTime < 0f) {
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+	[SerializeField] private float fireCooldown;
+	[SerializeField] private Laser laserPrefab;
+
+	private float fireCooldownLeft;
+
+	private void Update()
+	{
+		if (Input.GetButtonDown("Fire1") && fireCooldownLeft <= 0f) {
+			fireCooldownLeft = fireCooldown;
+
+			Laser laser = Instantiate(laserPrefab, transform.position, transform.rotation);
+		}
+
+		if (fireCooldownLeft > 0f) {
+			fireCooldownLeft -= Time.deltaTime;
+		}
+	}
+}
