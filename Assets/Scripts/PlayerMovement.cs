b1aa84b7b7184886ec34a32554a679ba73359870
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-19 14:57:01 +0100

    Player Dashing

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+	[SerializeField] private float dashSpeed;
+	[SerializeField] private float dashTime;
 
-	private Vector2 moveDir;
+	private Vector2 moveDir = Vector2.zero;
+	private bool isDashing = false;
+
     private Rigidbody2D rb;
 
 	private void Awake()
@@ -19,11 +24,31 @@ public class PlayerMovement : MonoBehaviour
 			Input.GetAxis("Horizontal"),
 			Input.GetAxis("Vertical")
 		);
+
+		// clamp diagonal movement speed
+		moveDir = Vector2.ClampMagnitude(moveDir, 1f);
+
+		if (!isDashing && Input.GetButtonDown("Dash")) {
+			StartCoroutine(Dash(moveDir));
+		}
 	}
 
 	private void FixedUpdate()
 	{
-		moveDir = Vector2.ClampMagnitude(moveDir, 1f);
-		rb.velocity = moveSpeed * moveDir;
+		if (!isDashing) {
+			rb.velocity = moveSpeed * moveDir;
+		}
+	}
+
+	private IEnumerator Dash(Vector2 dashDir)
+	{
+		isDashing = true;
+
+		for (float t = 0; t < dashTime; t += Time.deltaTime) {
+			rb.velocity = dashSpeed * dashDir;
+			yield return null;
+		}
+
+		isDashing = false;
 	}
 }
