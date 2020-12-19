cf6252074eec995b2bce04c513ceafbe29c01220
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-19 17:00:44 +0100

    Enemy state transition

--- Assets/Scripts/Enemy/MoveTowardsPlayer.cs
+++ Assets/Scripts/Enemy/MoveTowardsPlayer.cs
@@ -5,10 +5,17 @@ public class MoveTowardsPlayer : StateMachineBehaviour
     [SerializeField] private float moveSpeed;
     [SerializeField] private float stoppingDistance;
 
+    [Header("State Transition")]
+    [SerializeField] private float minStateTime;
+    [SerializeField] private float maxStateTime;
+    private float stateTimeLeft;
+
     private Transform player;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        stateTimeLeft = Random.Range(minStateTime, maxStateTime);
+
         player = GameObject.FindWithTag("Player").transform;
     }
 
@@ -17,6 +24,11 @@ public class MoveTowardsPlayer : StateMachineBehaviour
         Transform transform = animator.transform;
         Vector3 target = LerpByDistance(player.position, transform.position, stoppingDistance);
         transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+
+        stateTimeLeft -= Time.deltaTime;
+        if (stateTimeLeft <= 0f) {
+            animator.Play("RotateAroundPlayer");
+        }
     }
 
     private static Vector3 LerpByDistance(Vector3 from, Vector3 to, float dist)
--- Assets/Scripts/Enemy/RotateAroundPlayer.cs
+++ Assets/Scripts/Enemy/RotateAroundPlayer.cs
@@ -6,10 +6,17 @@ public class RotateAroundPlayer : StateMachineBehaviour
     [SerializeField] private float orbitDistance;
     [SerializeField] private float orbitRotateSpeed;
 
+    [Header("State Transition")]
+    [SerializeField] private float minStateTime;
+    [SerializeField] private float maxStateTime;
+    private float stateTimeLeft;
+
     private Transform player;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        stateTimeLeft = Random.Range(minStateTime, maxStateTime);
+
         player = GameObject.FindWithTag("Player").transform;
     }
 
@@ -19,6 +26,11 @@ public class RotateAroundPlayer : StateMachineBehaviour
         Vector3 angle = LerpByDistance(player.position, transform.position, orbitDistance);
         Vector3 target = RotatePointAroundPivot(angle, player.position);
         transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+
+        stateTimeLeft -= Time.deltaTime;
+        if (stateTimeLeft <= 0f) {
+            animator.Play("MoveTowardsPlayer");
+		}
     }
 
     private static Vector3 LerpByDistance(Vector3 from, Vector3 to, float dist)
