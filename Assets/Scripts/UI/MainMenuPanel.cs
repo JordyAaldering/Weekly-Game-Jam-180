8febe9b561043ed64cb4561c73a564e549218612
Jordy Aaldering <aalderingjordy@gmail.com>
2020-12-23 19:16:54 +0100

    Main menu

---
+++ Assets/Scripts/UI/MainMenuPanel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuPanel : MonoBehaviour
+{
+	[SerializeField] private AudioSource musicSoure;
+
+    public void StartLevel()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+
+	public void SetMusicVolume(float volume)
+	{
+		musicSoure.volume = volume;
+		PlayerPrefs.SetFloat("MusicVolume", volume);
+	}
+
+	public void SetSfxVolume(float volume)
+	{
+		PlayerPrefs.SetFloat("SfxVolume", volume);
+	}
+}
